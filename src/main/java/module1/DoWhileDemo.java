7e52a5d68bfa79248fe75640b4e39eea3d0f7fda
Cyndi Rader <janaidu02@gmial.com>
2016-12-31 22:00:24 +0000

    10th Phase commit point

---
+++ src/main/java/module1/DoWhileDemo.java
@@ -0,0 +1,20 @@
+package module1;
+
+// do-while is the ideal for this before it exit controlled
+public class DoWhileDemo {
+
+	private static final int NUMBERS_TO_PRINT = 20;
+	
+	public static void main(String[] args) {
+		int i= 0;
+		do
+		{
+			 i++ ;
+			System.out.println(i);
+		}while (i < NUMBERS_TO_PRINT);
+	}
+}
+
+// test 1
+//test 2
+// test3
