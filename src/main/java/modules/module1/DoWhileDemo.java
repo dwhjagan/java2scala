541d19f1efe4528ed216ff1f3fbe8ee75510fbec
Cyndi Rader <janaidu02@gmial.com>
2017-01-01 01:25:32 +0000

    10th Phase commit point

--- src/main/java/modules/module1/DoWhileDemo.java
+++ src/main/java/modules/module1/DoWhileDemo.java
@@ -2,16 +2,16 @@ package modules.module1;
 
 // do-while is the ideal for this before it exit controlled
 public class DoWhileDemo {
-
-	private static final int NUMBERS_TO_PRINT = 20;
+	private static final int NUMBERS_TO_PRINT = 5;
 	
 	public static void main(String[] args) {
+
 		int i= 0;
 		do
 		{
 			 i++ ;
 			System.out.println(i);
-		}while (i < NUMBERS_TO_PRINT);
+		} while (i < NUMBERS_TO_PRINT);
 	}
 }
 
--- src/main/java/modules/module1/NineTable.java
+++ src/main/java/modules/module1/NineTable.java
@@ -3,7 +3,7 @@ package modules.module1;
 public class NineTable {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <= 100; i++)
+		for (int i = 1; i <= 5; i++)
 		{
 			System.out.println("9 X " + i + " = " + 9*i);
 		}
