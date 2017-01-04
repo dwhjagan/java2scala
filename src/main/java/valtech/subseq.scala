f78a1dfab3040ea85df16ff1727e1a15255e175a
Cyndi Rader <janaidu02@gmial.com>
2017-01-04 08:08:50 +0000

    10th Phase commit point

--- src/main/java/valtech/subseq.scala
+++ src/main/java/valtech/subseq.scala
@@ -5,19 +5,20 @@ package valtech
   */
 
 
-object Array_Range {                             //Objects
-def main(args: Array[String]): Unit = {
+object Array_Range {
+  //Objects
+  def main(args: Array[String]): Unit = {
 
-}
-  def ksubsequences(k: Int, s: Array[Int]) = {
-    var p = Array.fill(k)(0)
-    p(0) = 1
-    var t = 0
-    for (n <- s) {
-      t += n
-      t %= k
-      p(t) += 1
+    def ksubsequences(k: Int, s: Array[Int]) = {
+      var p = Array.fill(k)(0)
+      p(0) = 1
+      var t = 0
+      for (n <- s) {
+        t += n
+        t %= k
+        p(t) += 1
+      }
+      p.map(x => x * (x - 1) / 2).sum
     }
-    p.map(x => x * (x - 1) / 2).sum
   }
 }
